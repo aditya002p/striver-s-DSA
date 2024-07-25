0d27fd9371197b97a43f5568880198fe3084bd90
Aditya <ricky.pathak902@gmail.com>
2024-07-25 22:16:23 +0530

    learning pattern questions

--- learning1.cpp
+++ learning1.cpp
@@ -472,3 +472,98 @@ and prints the corresponding day of the week
 //    cout << "value inside main : " << arr[1] << endl;
 //    return 0;
 // }
+
+// ----------Pattern questions------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Pattern 1
+
+// void pattern1(int n)
+// {
+//    for (int i = 0; i < n; i++)
+//    {
+//       for (int j = 0; j < n; j++)
+//       {
+//          cout << "* ";
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern1(n);
+//    return 0;
+// }
+
+// Pattern 2
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern2(int n)
+// {
+//    for (int i = 0; i < n; i++)
+//    {
+//       for (int j = 0; j <= i; j++)
+//       {
+//          cout << "*";
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern2(n);
+//    return 0;
+// }
+
+// Pattern 3
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern3(int n)
+// {
+//    for (int i = 1; i <= n; i++)
+//    {
+//       for (int j = 1; j <= i; j++)
+//       {
+//          cout << j;
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern3(n);
+//    return 0;
+// }
+
+// Pattern 4
+#include <bits/stdc++.h>
+using namespace std;
+
+void pattern4(int n)
+{
+   for (int i = 1; i <= n; i++)
+   {
+      for (int j = 1; j <= i; j++)
+      {
+         cout << i;
+      }
+      cout << endl;
+   }
+}
+int main()
+{
+   int n;
+   cin >> n;
+   pattern4(n);
+   return 0;
+}
