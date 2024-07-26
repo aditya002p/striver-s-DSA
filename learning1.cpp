5735a9c8982ae8545d79d5f6064fb4c7566971bc
Aditya <ricky.pathak902@gmail.com>
2024-07-26 17:47:46 +0530

    pattern learning

--- learning1.cpp
+++ learning1.cpp
@@ -475,8 +475,8 @@ and prints the corresponding day of the week
 
 // ----------Pattern questions------------------
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
 // Pattern 1
 
@@ -546,24 +546,237 @@ using namespace std;
 // }
 
 // Pattern 4
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern4(int n)
+// {
+//    for (int i = 1; i <= n; i++)
+//    {
+//       for (int j = 1; j <= i; j++)
+//       {
+//          cout << i;
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern4(n);
+//    return 0;
+// }
+
+// Pattern 5
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern5(int n)
+// {
+//    for (int i = 1; i < n; i++)
+//    {
+//       for (int j = n; j >= i; j--)
+//       {
+//          cout << "*";
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern5(n);
+//    return 0;
+// }
+
+// pattern 7
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern7(int n)
+// {
+
+//    for (int i = 0; i < n; i++)
+//    {
+//       // space
+//       for (int j = 0; j < n - i - 1; j++)
+//       {
+//          cout << " ";
+//       }
+//       // star
+//       for (int j = 0; j < 2 * i + 1; j++)
+//       {
+//          cout << "*";
+//       }
+//       // space
+//       for (int j = 0; j < n - i - 1; j++)
+//       {
+//          cout << " ";
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern7(n);
+//    return 0;
+// }
+
+// pattern 8
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern8(int n)
+// {
+
+//    for (int i = 0; i < n; i++)
+//    {
+//       // space
+//       for (int j = 0; j < i; j++)
+//       {
+//          cout << " ";
+//       }
+//       // star
+//       for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+//       {
+//          cout << "*";
+//       }
+//       // space
+//       for (int j = 0; j < i; j++)
+//       {
+//          cout << " ";
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern8(n);
+//    return 0;
+// }
+
+// pattern 9
+// #include <bits/stdc++.h>
+// using namespace std;
+// void pattern7(int n)
+// {
+
+//    for (int i = 0; i < n; i++)
+//    {
+//       // space
+//       for (int j = 0; j < n - i - 1; j++)
+//       {
+//          cout << " ";
+//       }
+//       // star
+//       for (int j = 0; j < 2 * i + 1; j++)
+//       {
+//          cout << "*";
+//       }
+//       // space
+//       for (int j = 0; j < n - i - 1; j++)
+//       {
+//          cout << " ";
+//       }
+//       cout << endl;
+//    }
+// }
+// void pattern8(int n)
+// {
+
+//    for (int i = 0; i < n; i++)
+//    {
+//       // space
+//       for (int j = 0; j < i; j++)
+//       {
+//          cout << " ";
+//       }
+//       // star
+//       for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+//       {
+//          cout << "*";
+//       }
+//       // space
+//       for (int j = 0; j < i; j++)
+//       {
+//          cout << " ";
+//       }
+//       cout << endl;
+//    }
+// }
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern7(n);
+//    pattern8(n);
+//    return 0;
+// }
+
+// pattern 10
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void pattern10(int n)
+// {
+//    for (int i = 1; i <= n; i++)
+//    {
+//       for (int j = 1; j <= i; j++)
+//       {
+//          cout << "*";
+//       }
+//       cout << endl;
+//    }
+//    for (int i = n - 1; i >= 1; i--)
+//    {
+//       for (int j = 1; j <= i; j++)
+//       {
+//          cout << "*";
+//       }
+//       cout << endl;
+//    }
+// }
+
+// int main()
+// {
+//    int n;
+//    cin >> n;
+//    pattern10(n);
+//    return 0;
+// }
+
+// pattern 10 (new way)
+
 #include <bits/stdc++.h>
 using namespace std;
 
-void pattern4(int n)
+void pattern10(int n)
 {
-   for (int i = 1; i <= n; i++)
+   for (int i = 1; i <= 2 * n - 1; i++)
    {
-      for (int j = 1; j <= i; j++)
+      int stars = i;
+      if (i > n)
+         stars = 2 * n - i;
+      for (int j = 1; j <= stars; j++)
       {
-         cout << i;
+         cout << "*";
       }
       cout << endl;
    }
 }
+
 int main()
 {
    int n;
    cin >> n;
-   pattern4(n);
+   pattern10(n);
    return 0;
 }
